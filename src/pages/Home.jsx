838694a471eecb13835563e038b60e3a82c39395
rushi1225 <rushibhingare25@gmail.com>
2025-01-29 14:51:42 +0530

    home create

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Stage, Layer, Rect, Circle } from 'react-konva';
+
+const Home = () => {
+
+    const [shapes, setShapes]=useState([])
+
+  return (
+      <>
+    {shapes.length==0 ?(
+
+    ): ( <>
+    
+    </> ) }
+    
+         
+    </>
+  )
+}
+
+export default Home
